bd67f28f5c39bba347933a6fe85fb9a30c3f67b7
Alan Corleto <corletoalan@gmail.com>
2026-09-07 19:04:05 -0300

    Update payment information in guest player creation screen

--- scenes/screens/tournament/guest_player/create_guest_player_screen.gd
+++ scenes/screens/tournament/guest_player/create_guest_player_screen.gd
@@ -2,6 +2,8 @@ extends GuestPlayerScreenBase
 
 
 func _submit_form() -> bool:
+	# Guest player creation
+	
 	var guest_player_create := GuestPlayerCreate.new()
 	
 	guest_player_create.nickname = nickname_line_edit.text
@@ -13,6 +15,22 @@ func _submit_form() -> bool:
 	)
 	
 	if not player:
+		return false
+	
+	# Has paid
+	
+	var player_in_tournament_update := PlayerInTournamentUpdate.new()
+	player_in_tournament_update.has_paid_entry = has_paid_check_box.button_pressed
+	
+	var update_player_in_tournament: PlayerInTournament = (
+		await TournamentsRouter.update_player_in_tournament(
+			Globals.current_tournament.id,
+			player.id,
+			player_in_tournament_update,
+		)
+	)
+	
+	if not update_player_in_tournament:
 		return false
 	
 	return true
