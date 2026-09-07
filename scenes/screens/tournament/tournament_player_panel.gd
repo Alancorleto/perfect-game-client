62e2873ca550f6d7fca46138561e23799e0e9e18
Alan Corleto <corletoalan@gmail.com>
2026-09-07 19:38:57 -0300

    Display payment check for every player in tournament screen

--- scenes/screens/tournament/tournament_player_panel.gd
+++ scenes/screens/tournament/tournament_player_panel.gd
@@ -7,10 +7,15 @@ signal pressed(player_in_tournament: PlayerInTournament)
 @onready var name_panel: Label = %NamePanel
 @onready var paid_check_button: Button = %PaidCheckButton
 @onready var delete_button: TextureButton = %DeleteButton
+@onready var has_paid_container: HBoxContainer = %HasPaidContainer
 
 
 func populate(player_in_tournament: PlayerInTournament) -> void:
 	name_panel.text = player_in_tournament.player.nickname
+	if Globals.organizer_mode_enabled and player_in_tournament.has_paid_entry:
+		has_paid_container.show()
+	else:
+		has_paid_container.hide()
 	button.pressed.connect(_notify_pressed.bind(player_in_tournament))
 
 
